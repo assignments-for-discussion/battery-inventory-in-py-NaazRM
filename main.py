d3a622a7306556f5a31c0af73f3ee679d7c35f3b
NaazRM <120334558+NaazRM@users.noreply.github.com>
2023-12-03 17:05:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 
 def count_batteries_by_health(present_capacities):
-  return {
-    "healthy": 0,
-    "exchange": 0,
-    "failed": 0
-  }
+  # Initialize counts
+        "healthy": 0,
+        "exchange": 0,
+        "failed": 0
+    }
+
+    # Rated capacity
+    rated_capacity = 120
+
+    # Classify based on SoH
+    for capacity in present_capacities:
+        soh = (capacity / rated_capacity) * 100
+
+        if soh > 80:
+            counts["healthy"] += 1
+        elif 62 <= soh <= 80:
+            counts["exchange"] += 1
+        else:
+            counts["failed"] += 1
+
+    return counts
 
 
 def test_bucketing_by_health():
