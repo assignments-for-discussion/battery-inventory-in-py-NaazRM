44dffe2bd8f0c0a4345637e4f4f8ed001fd11575
NaazRM <120334558+NaazRM@users.noreply.github.com>
2023-12-03 17:13:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,10 +26,24 @@ def count_batteries_by_health(present_capacities):
 def test_bucketing_by_health():
   print("Counting batteries by SoH...\n")
   present_capacities = [113, 116, 80, 95, 92, 70]
+  # Test 1: Standard test
   counts = count_batteries_by_health(present_capacities)
   assert(counts["healthy"] == 2)
   assert(counts["exchange"] == 3)
   assert(counts["failed"] == 1)
+  # Test Case 2: Boundary condition - minimum SoH
+  present_capacities = [0]
+  counts = count_batteries_by_health(present_capacities)
+  assert(counts["healthy"] == 0)
+  assert(counts["exchange"] == 0)
+  assert(counts["failed"] == 1)
+
+  # Test Case 3: Boundary condition - maximum SoH
+  present_capacities = [120]
+  counts = count_batteries_by_health(present_capacities)
+  assert(counts["healthy"] == 1)
+  assert(counts["exchange"] == 0)
+  assert(counts["failed"] == 0)
   print("Done counting :)")
 
 
