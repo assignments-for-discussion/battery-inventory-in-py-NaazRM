372918a8b2b5405771a4165e5b30985815102f34
NaazRM <120334558+NaazRM@users.noreply.github.com>
2023-12-03 17:47:22 +0530

    Update main.py
    program completed with boundary conditions

--- main.py
+++ main.py
@@ -28,8 +28,9 @@ def count_batteries_by_health(present_capacities):
 
 def test_bucketing_by_health():
   print("Counting batteries by SoH...\n")
-  present_capacities = [113, 116, 80, 95, 92, 70]
+    
   # Test 1: Standard test
+  present_capacities = [113, 116, 80, 95, 92, 70]
   counts = count_batteries_by_health(present_capacities)
   assert(counts["healthy"] == 2)
   assert(counts["exchange"] == 3)
